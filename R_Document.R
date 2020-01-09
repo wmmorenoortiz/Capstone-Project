b6e6f39e48e20905c61149e24211346464c7b78a
wmmorenoortiz <udlawilsonmoreno@gmail.com>
2020-01-09 17:12:36 +0000

    12:12 # 09_01_2020

--- R_Document.R
+++ R_Document.R
@@ -1,7 +1,10 @@
 # require package
-# if (!require(package)) install.packages('pastecs')
 # if (!require(package)) install.packages('psych')
-# library(pastecs)
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+library(caret)
+library(tidyverse)
 # library(ggplot2)
 # library(tidyverse)
 # library(psych)
@@ -15,14 +18,7 @@ head(database)
 
 ### Summay Statistics
 
-t <- describeBy(database,
-           digits= 2)
-h = data.frame(t$Female)
-round(h, 2)
-
-ext <- with(database.gathered, Map(function(x) ave(value, Gender, FUN=x), 
-                    list(avg=mean, min=min, max=max, SD=sd)))
-cbind(database.gathered, ext)
+describeBy(database, digits= 2)
 
 ### Visualization
 database.gathered <- database %>% as.data.frame() %>% 
@@ -46,3 +42,47 @@ ggplot(data = database.gathered , mapping = aes(sample = value, color = Gender))
   stat_qq() +  stat_qq_line(color = "black") +
   facet_wrap(facets =  vars(variable )) 
 
+
+### **Step 3:**  Split the database in training and testing
+
+#### Choose only the variables of interest.
+#new_edx <- edx %>% select(rating,userId,movieId)
+#head(new_edx)
+
+#### Split the database in training and testing
+set.seed(755)
+test_index <- createDataPartition(y = database$Dataset, times = 1,
+                                  p = 0.2, list = FALSE)
+
+train_set <- database[-test_index,]
+test_set <- database[test_index,]
+
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+names(train_set)
+
+# Step  4: Choose the Model and train the model with the training base
+train_set$Dataset[train_set$Dataset == 1] = 1
+train_set$Dataset[train_set$Dataset == 2] = 0
+train_set$Gender <- as.factor(train_set$Gender)
+
+
+test_set$Dataset[test_set$Dataset == 1] = 1
+test_set$Dataset[test_set$Dataset == 2] = 0
+test_set$Gender <- as.factor(test_set$Gender)
+
+
+
+mol <- glm(Dataset ~ Age + Alamine_Aminotransferase , data = train_set, family = binomial() )
+summary(mol)
+
+y_hat <- predict(mol,test_set,type = "response")
+glm.pred  <- ifelse(y_hat > 0.5, "1", "0")
+
+table(glm.pred,test_set$Dataset )
+
+mean(glm.pred == test_set$Dataset)
+
+cor(database[,-2])
